e049d82f27e1667840fd54b19b626f5b1aeb3e4f
Prathiksha73 <kushiprathikshay@gmail.com>
2026-01-07 10:34:25 +0530

    my local changes

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 from django.contrib.auth.models import AbstractUser
 
 class User(AbstractUser):
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+<<<<<<< Updated upstream
 # Create your views here.
+=======
+>>>>>>> Stashed changes
 from rest_framework_simplejwt.views import TokenObtainPairView
 from .serializers import MyTokenObtainPairSerializer
 
