a6a3fbccd05647916425c99cc44252a22c38f326
Yasinm123 <yaseenpcb77@gmail.com>
2026-01-07 14:28:53 +0530

    Update Models

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import DoctorProfile
 
-# Register your models here.
+@admin.register(DoctorProfile)
+class DoctorProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'specialization', 'created_at')
+    search_fields = ('user__username', 'specialization')
