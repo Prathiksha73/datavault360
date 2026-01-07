f0fc5fee22daed1dbf421f83a7387fbb5976f781
Yasinm123 <yaseenpcb77@gmail.com>
2026-01-07 12:57:03 +0530

    Update models.py

--- backend/api/models.py
+++ backend/api/models.py
@@ -22,3 +22,17 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+from django.contrib.auth import get_user_model
+User = get_user_model()
+
+class DoctorProfile(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        related_name='doctor_profile'
+    )
+    specialization = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Dr. {self.user.username}"
